f95c56f0f50eb42e24a7eb50ba39d77b7d4747bc
ShenLong <93514880+LongDang777@users.noreply.github.com>
2022-05-04 21:26:22 +0700

    Update QuanLyPhimService.js
    fix

--- src/services/QuanLyPhimService.js
+++ src/services/QuanLyPhimService.js
@@ -1,8 +1,28 @@
 import { http } from "../util/setting";
-
-class QuanLyPhimService{
-    layDanhSachPhim = ()=> {
-        return http.get('/api/QuanLyPhim/LayDanhSachPhim?maNhom=GP03')
+import { GROUP_ID } from "./TypeService";
+// gfdsgdsfgs
+class QuanLyPhimService {
+  layDanhSachPhim = (tenPhim) => {
+    if (tenPhim.trim() != '') {
+      return http.get(`/api/QuanLyPhim/LayDanhSachPhim?maNhom=${GROUP_ID}&tenPhim=${tenPhim}`)
     }
+    return http.get(`/api/QuanLyPhim/LayDanhSachPhim?maNhom=${GROUP_ID}`)
+  };
+
+  themPhimUploadHinh = (formData) => {
+    return http.post(`/api/QuanLyPhim/ThemPhimUploadHinh`, formData)
+  }
+
+  layThongTinPhim = (maPhim) => {
+    return http.get(`/api/QuanLyPhim/LayThongTinPhim?MaPhim=${maPhim}`)
+  };
+
+  capNhatPhimUpload = (formData) => {
+    return http.post(`/api/QuanLyPhim/CapNhatPhimUpload`, formData)
+  };
+
+  xoaPhim = (maPhim) => { return http.delete(`/api/QuanLyPhim/XoaPhim?MaPhim=${maPhim}`) };
+
 }
-export const quanLyPhimService = new QuanLyPhimService()
+
+export const quanLyPhimService = new QuanLyPhimService();
