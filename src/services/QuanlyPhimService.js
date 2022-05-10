b9a0836fa9f5b3f7f310931aa1a1c2e4b0bb0163
Long <dangtienlong2020@gmail.com>
2022-05-10 15:19:00 +0700

    fix

--- src/services/QuanlyPhimService.js
+++
@@ -1,8 +0,0 @@
-import { http } from "../util/setting";
-
-class QuanLyPhimService{
-    layDanhSachPhim = ()=> {
-        return http.get('/api/QuanLyPhim/LayDanhSachPhim?maNhom=GP03')
-    }
-}
-export const quanLyPhimService = new QuanLyPhimService()
