1b7df15707e1af44f3e68bf49cfed27efc392d31
Long <dangtienlong2020@gmail.com>
2022-05-10 15:12:46 +0700

    fix

--- src/services/QuanlyPhimService.js
+++ src/services/QuanlyPhimService.js
@@ -1,28 +1,8 @@
 import { http } from "../util/setting";
-import { GROUP_ID } from "./TypeService";
 
-class QuanLyPhimService {
-  layDanhSachPhim = (tenPhim) => {
-    if (tenPhim) {
-      return http.get(`/api/QuanLyPhim/LayDanhSachPhim?maNhom=${GROUP_ID}&tenPhim=${tenPhim}`)
+class QuanLyPhimService{
+    layDanhSachPhim = ()=> {
+        return http.get('/api/QuanLyPhim/LayDanhSachPhim?maNhom=GP03')
     }
-    return http.get(`/api/QuanLyPhim/LayDanhSachPhim?maNhom=${GROUP_ID}`)
-  };
-
-  themPhimUploadHinh = (formData) => {
-    return http.post(`/api/QuanLyPhim/ThemPhimUploadHinh`, formData)
-  }
-
-  layThongTinPhim = (maPhim) => {
-    return http.get(`/api/QuanLyPhim/LayThongTinPhim?MaPhim=${maPhim}`)
-  };
-
-  capNhatPhimUpload = (formData) => {
-    return http.post(`/api/QuanLyPhim/CapNhatPhimUpload`, formData)
-  };
-
-  xoaPhim = (maPhim) => { return http.delete(`/api/QuanLyPhim/XoaPhim?MaPhim=${maPhim}`) };
-
 }
-
-export const quanLyPhimService = new QuanLyPhimService();
+export const quanLyPhimService = new QuanLyPhimService()
